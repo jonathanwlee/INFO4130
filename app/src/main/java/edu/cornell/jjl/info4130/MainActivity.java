144142dace5debd8d3e8f841027d3905d6d0e892
jonathanwlee <jwl274@cornell.edu>
2015-11-12 22:46:27 -0500

    Update

---
+++ app/src/main/java/edu/cornell/jjl/info4130/MainActivity.java
@@ -0,0 +1,251 @@
+package edu.cornell.jjl.info4130;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import com.philips.lighting.hue.sdk.PHAccessPoint;
+import com.philips.lighting.hue.sdk.PHBridgeSearchManager;
+import com.philips.lighting.hue.sdk.PHHueSDK;
+import com.philips.lighting.hue.sdk.PHMessageType;
+import com.philips.lighting.hue.sdk.PHSDKListener;
+import com.philips.lighting.model.PHBridge;
+import com.philips.lighting.model.PHHueError;
+import com.philips.lighting.model.PHHueParsingError;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
+    private PHHueSDK phHueSDK;
+
+    private HueSharedPreferences prefs;
+    private AccessPointListAdapter adapter;
+
+    private boolean lastSearchWasIPScan = false;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        phHueSDK = PHHueSDK.create();
+        phHueSDK.setAppName("INFO4130");
+        phHueSDK.setDeviceName(android.os.Build.MODEL);
+        phHueSDK.getNotificationManager().registerSDKListener(listener);
+
+        adapter = new AccessPointListAdapter(getApplicationContext(), phHueSDK.getAccessPointsFound());
+
+        ListView accessPointList = (ListView) findViewById(R.id.bridge_list);
+        accessPointList.setOnItemClickListener(this);
+        accessPointList.setAdapter(adapter);
+
+        // Try to automatically connect to the last known bridge.  For first time use this will be empty so a bridge search is automatically started.
+        prefs = HueSharedPreferences.getInstance(getApplicationContext());
+        String lastIpAddress   = prefs.getLastConnectedIPAddress();
+        String lastUsername    = prefs.getUsername();
+
+        // Automatically try to connect to the last connected IP Address.  For multiple bridge support a different implementation is required.
+        if (lastIpAddress !=null && !lastIpAddress.equals("")) {
+            PHAccessPoint lastAccessPoint = new PHAccessPoint();
+            lastAccessPoint.setIpAddress(lastIpAddress);
+            lastAccessPoint.setUsername(lastUsername);
+
+            if (!phHueSDK.isAccessPointConnected(lastAccessPoint)) {
+                PHWizardAlertDialog.getInstance().showProgressDialog(R.string.connecting, MainActivity.this);
+                phHueSDK.connect(lastAccessPoint);
+            }
+        }
+        else {  // First time use, so perform a bridge search.
+            doBridgeSearch();
+        }
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doBridgeSearch();
+
+            }
+        });
+    }
+
+    // Local SDK Listener
+    private PHSDKListener listener = new PHSDKListener() {
+
+        @Override
+        public void onAccessPointsFound(List<PHAccessPoint> accessPoint) {
+            Log.w("Debug", "Access Points Found. " + accessPoint.size());
+
+            PHWizardAlertDialog.getInstance().closeProgressDialog();
+            if (accessPoint != null && accessPoint.size() > 0) {
+                phHueSDK.getAccessPointsFound().clear();
+                phHueSDK.getAccessPointsFound().addAll(accessPoint);
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        adapter.updateData(phHueSDK.getAccessPointsFound());
+                    }
+                });
+
+            }
+
+        }
+
+        @Override
+        public void onCacheUpdated(List<Integer> arg0, PHBridge bridge) {
+            Log.w("Debug", "On CacheUpdated");
+
+        }
+
+        @Override
+        public void onBridgeConnected(PHBridge b, String username) {
+            phHueSDK.setSelectedBridge(b);
+            phHueSDK.enableHeartbeat(b, PHHueSDK.HB_INTERVAL);
+            phHueSDK.getLastHeartbeat().put(b.getResourceCache().getBridgeConfiguration() .getIpAddress(), System.currentTimeMillis());
+            prefs.setLastConnectedIPAddress(b.getResourceCache().getBridgeConfiguration().getIpAddress());
+            prefs.setUsername(username);
+            PHWizardAlertDialog.getInstance().closeProgressDialog();
+            startMainActivity();
+        }
+
+        @Override
+        public void onAuthenticationRequired(PHAccessPoint accessPoint) {
+            Log.w("Debug", "Authentication Required.");
+            phHueSDK.startPushlinkAuthentication(accessPoint);
+            startActivity(new Intent(MainActivity.this, PHPushlinkActivity.class));
+
+        }
+
+        @Override
+        public void onConnectionResumed(PHBridge bridge) {
+            if (MainActivity.this.isFinishing())
+                return;
+
+            Log.v("Debug", "onConnectionResumed" + bridge.getResourceCache().getBridgeConfiguration().getIpAddress());
+            phHueSDK.getLastHeartbeat().put(bridge.getResourceCache().getBridgeConfiguration().getIpAddress(),  System.currentTimeMillis());
+            for (int i = 0; i < phHueSDK.getDisconnectedAccessPoint().size(); i++) {
+
+                if (phHueSDK.getDisconnectedAccessPoint().get(i).getIpAddress().equals(bridge.getResourceCache().getBridgeConfiguration().getIpAddress())) {
+                    phHueSDK.getDisconnectedAccessPoint().remove(i);
+                }
+            }
+
+        }
+
+        @Override
+        public void onConnectionLost(PHAccessPoint accessPoint) {
+            Log.v("Debug", "onConnectionLost : " + accessPoint.getIpAddress());
+            if (!phHueSDK.getDisconnectedAccessPoint().contains(accessPoint)) {
+                phHueSDK.getDisconnectedAccessPoint().add(accessPoint);
+            }
+        }
+
+        @Override
+        public void onError(int code, final String message) {
+            Log.e("Debug", "on Error Called : " + code + ":" + message);
+
+            if (code == PHHueError.NO_CONNECTION) {
+                Log.w("Debug", "On No Connection");
+            }
+            else if (code == PHHueError.AUTHENTICATION_FAILED || code==PHMessageType.PUSHLINK_AUTHENTICATION_FAILED) {
+                PHWizardAlertDialog.getInstance().closeProgressDialog();
+            }
+            else if (code == PHHueError.BRIDGE_NOT_RESPONDING) {
+                Log.w("Debug", "Bridge Not Responding . . . ");
+                PHWizardAlertDialog.getInstance().closeProgressDialog();
+                MainActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        PHWizardAlertDialog.showErrorDialog(MainActivity.this, message, R.string.btn_ok);
+                    }
+                });
+
+            }
+            else if (code == PHMessageType.BRIDGE_NOT_FOUND) {
+
+                if (!lastSearchWasIPScan) {  // Perform an IP Scan (backup mechanism) if UPNP and Portal Search fails.
+                    phHueSDK = PHHueSDK.getInstance();
+                    PHBridgeSearchManager sm = (PHBridgeSearchManager) phHueSDK.getSDKService(PHHueSDK.SEARCH_BRIDGE);
+                    sm.search(false, false, true);
+                    lastSearchWasIPScan=true;
+                }
+                else {
+                    PHWizardAlertDialog.getInstance().closeProgressDialog();
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            PHWizardAlertDialog.showErrorDialog(MainActivity.this, message, R.string.btn_ok);
+                        }
+                    });
+                }
+
+
+            }
+        }
+
+        @Override
+        public void onParsingErrors(List<PHHueParsingError> parsingErrorsList) {
+            for (PHHueParsingError parsingError: parsingErrorsList) {
+                Log.e("Debug", "ParsingError : " + parsingError.getMessage());
+            }
+        }
+    };
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (listener !=null) {
+            phHueSDK.getNotificationManager().unregisterSDKListener(listener);
+        }
+        phHueSDK.disableAllHeartbeat();
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        PHAccessPoint accessPoint = (PHAccessPoint) adapter.getItem(position);
+
+        PHBridge connectedBridge = phHueSDK.getSelectedBridge();
+
+        if (connectedBridge != null) {
+            String connectedIP = connectedBridge.getResourceCache().getBridgeConfiguration().getIpAddress();
+            if (connectedIP != null) {   // We are already connected here:-
+                phHueSDK.disableHeartbeat(connectedBridge);
+                phHueSDK.disconnect(connectedBridge);
+            }
+        }
+        PHWizardAlertDialog.getInstance().showProgressDialog(R.string.connecting, MainActivity.this);
+        phHueSDK.connect(accessPoint);
+    }
+
+
+    public void doBridgeSearch() {
+        PHWizardAlertDialog.getInstance().showProgressDialog(R.string.search_progress, MainActivity.this);
+        PHBridgeSearchManager sm = (PHBridgeSearchManager) phHueSDK.getSDKService(PHHueSDK.SEARCH_BRIDGE);
+        // Start the UPNP Searching of local bridges.
+        sm.search(true, true);
+    }
+
+    // Starting the main activity this way, prevents the PushLink Activity being shown when pressing the back button.
+    public void startMainActivity() {
+        Intent intent = new Intent(getApplicationContext(), StartScreen.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
+            intent.addFlags(0x8000); // equal to Intent.FLAG_ACTIVITY_CLEAR_TASK which is only available from API level 11
+        startActivity(intent);
+    }
+
+}
